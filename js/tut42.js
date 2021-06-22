ee7a804bd8f84b39561e47209e85c8bb8f31231b
msdesai1499 <msdesai@mitaoe.ac.in>
2021-06-22 22:41:45 +0530

    commit

--- js/tut42.js
+++ js/tut42.js
@@ -3,17 +3,43 @@ console.log('This is tut 42');
 let myBtn = document.getElementById('myBtn');
 let content = document.getElementById('content');
 
+// function getData() {
+// 	console.log("Starting Get Data");
+// 	url = "abc.txt";
+// 	fetch(url).then((response) => {
+// 		console.log("Inside First then");
+// 		return response.text();
+// 	}).then((data) => {
+// 		console.log("Inside Second then");
+// 		console.log(data);
+// 	})
+// }
 function getData() {
 	console.log("Starting Get Data");
-	url = "abc.txt";
+	url = "https://api.github.com/users";
 	fetch(url).then((response) => {
 		console.log("Inside First then");
-		return response.text();
+		return response.json();
 	}).then((data) => {
 		console.log("Inside Second then");
 		console.log(data);
 	})
 }
+function postData() {
+
+	url = "https://dummy.restapiexample.com/api/v1/create";
+
+	data = `{"name":"Mandar","salary":"123","age":"23"}`
+	params = {
+		method: 'post',
+		headers: {
+			'Content-type': 'application/json'
+		},
+		body: data
+	}
+	fetch(url, params).then(response => response.json())
+		.then(data => console.log(data))
+}
 console.log("Before Get Data");
-getData();
+postData();
 console.log("After Get Data");
